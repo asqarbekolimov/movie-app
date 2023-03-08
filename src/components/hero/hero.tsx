d01bb11a89d75cfe1c705332d24402211e90169d
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-03-08 15:15:08 +0500

    review avarage

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { HeroProps } from "./hero.props";
 import { TbPlayerPlay } from "react-icons/tb";
+import ReactStars from "react-stars";
 
 const Hero = ({ trending }: HeroProps): JSX.Element => {
   const [movie, setMovie] = useState<IMovie>({} as IMovie);
@@ -23,12 +24,25 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
           className="object-cover"
         />
       </div>
+      <div className="flex items-center space-x-2">
+        <ReactStars
+          edit={false}
+          count={10}
+          value={movie.vote_average}
+          color2={"#fff"}
+        />
+        <p>({movie.vote_count})</p>
+      </div>
+
+      <div className="py-[4px] px-[8px] text-center rounded-bl-[8px] rounded-tr-[8px] bg-[#1d1d1d]/50 w-[111px]">
+        {movie.media_type}
+      </div>
 
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
       <p className="max-w-xs md:max-w-lg lg:max-w-2xl text-xs text-shadow-md md:text-lg lg:text-2xl">
-        {movie?.overview}
+        {movie?.overview?.slice(0, 100)}...
       </p>
 
       <div>
